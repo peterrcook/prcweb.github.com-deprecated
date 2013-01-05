92be83465f8404fa42acdf28ddfdfcef08bb972f
Peter Cook <peterrcook@yahoo.co.uk>
2013-01-05 11:48:39 +0000

    Add basic content from static mockup

---
+++ js/site.js
@@ -0,0 +1,21 @@
+/*$('.showcase .rollover').on('mouseover', function() {
+/*	$(this).animate({'opacity': 0.7}, 200);
+	$(this).animate({'opacity': 0.8}, 200);
+});
+$('.showcase .rollover').on('mouseout', function() {
+	//$(this).animate({'opacity': 1}, 200);
+	$(this).animate({'opacity': 0}, 200);
+});
+
+
+/*
+$('.showcase rollover').on('mouseover', function() {
+/*	$(this).animate({'opacity': 0.7}, 200);
+	$(this).parent().find('.rollover').animate({'opacity': 0.8}, 200);
+});
+$('.showcase img').on('mouseout', function() {
+	//$(this).animate({'opacity': 1}, 200);
+	$(this).parent().find('.rollover').animate({'opacity': 0}, 200);
+});
+
+*/
